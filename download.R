6ecdf6e8a596ed5e93bf3b0734dca51aa90a8a49
unknown <mislav.sagovac@contentio.biz>
2023-10-10 16:50:24 +0200

    change data name

--- download.R
+++ download.R
@@ -6,5 +6,4 @@ endpoint = "https://snpmarketdata.blob.core.windows.net/"
 BLOBENDPOINT = storage_endpoint(endpoint, key=blob_key)
 cont = storage_container(BLOBENDPOINT, "jphd")
 # storage_download(cont, "pead-predictors-sample.csv", overwrite=TRUE) # uncomment this for sample
-storage_download(cont, "pead-predictors.csv", overwrite=TRUE) # uncomment this for full dataset
-storage_download(cont, "pead-predictors-sample.csv", overwrite=TRUE)
+storage_download(cont, "pead-predictors-update.csv", overwrite=TRUE) # uncomment this for full dataset
