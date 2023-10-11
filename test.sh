df20f5b9b16f126cfc3d72b51cefcd22714a474c
unknown <mislav.sagovac@contentio.biz>
2023-10-11 11:47:52 +0200

    add test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 ## export APPTAINERENV_LD_LIBRARY_PATH="${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}:\$LD_LIBRARY_PATH:/opt/cray/pe/pals/1.2.3/lib"
 
-apptainer exec --bind image.sif Rscript -e 'Sys.time()'
-apptainer exec --bind /opt,/run image.sif Rscript -e 'Sys.which("qsub")'
+apptainer exec image.sif Rscript -e 'Sys.time()'
+# apptainer exec --bind /opt,/run image.sif Rscript -e 'Sys.which("qsub")'
